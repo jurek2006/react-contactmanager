b7e0c847cee243095f50b8c8c39cbd4c2ac60731
Jurek <jurek2006@gmail.com>
2018-09-10 11:01:12 +0200

    Zakończenie sekcji 4 - State & The Context API. Po video 26. Adding A Context Reducer For Actions

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Contacts from "./components/Contacts";
+import Header from "./components/Header";
+
+import { Provider } from "./context";
+
+class App extends Component {
+    render() {
+        return (
+            <Provider>
+                <div className="App">
+                    <Header branding="Contact Manager" />
+                    <div className="container">
+                        <Contacts />
+                    </div>
+                </div>
+            </Provider>
+        );
+    }
+}
+
+export default App;
